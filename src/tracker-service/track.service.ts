8d0f13b2a253d64898725ea6a6db1ca3b8366202
charliedevcode <charliedevcode@gmail.com>
2023-06-15 15:07:18 +0200

    item tracking v0.1

--- src/tracker-service/track.service.ts
+++ src/tracker-service/track.service.ts
@@ -2,57 +2,100 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AxiosError } from 'axios';
 import { catchError, firstValueFrom } from 'rxjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TrackedItem } from '../database/entities/tracked-items/tracked-items.entity';
+import { AxiosError } from 'axios';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TrackService {
   private readonly logger = new Logger(TrackService.name);
-
-  // We will store the tracked items here
-  private trackedItems: Map<string, Item> = new Map();
-
+  private trackedItems: TrackedItem[];
+  private cronEnabled = false;
   constructor(
     private readonly httpService: HttpService,
     private readonly config: ConfigService,
-  ) {}
+    @InjectRepository(TrackedItem)
+    private trackedItemsRepository: Repository<TrackedItem>,
+  ) {
+    this._loadTrackedItems(); // Do this after web3 has been initialized up to the latest block.
+  }
+
+  private async _loadTrackedItems(): Promise<void> {
+    this.trackedItems = await this.trackedItemsRepository.find();
+    this.cronEnabled = true;
+  }
 
   // To start tracking an item
-  async trackItem(_steamId64: string, assetId: string): Promise<void> {
-    const inventory = await this.getInventory(_steamId64);
-    const item = inventory.find((item) => item.assetid === assetId);
+   async trackItem(originId: string, destinationId: string, assetId: string): Promise<void> {
+    const originInventory = await this.getInventory(originId);
+    const item = originInventory.find((item: { assetid: string; }) => item.assetid === assetId);
 
     if (item) {
-      this.trackedItems.set(assetId, {
-        floatValue: item.floatvalue,
-        paintSeed: item.paintseed,
-        paintIndex: item.paintindex,
-        originId: _steamId64,
+      const targetInventory = await this.getInventory(destinationId);
+      
+      const similarItemsCount = targetInventory.filter((targetItem: targetItem) =>
+        targetItem.market_hash_name === item.market_hash_name &&
+        targetItem.floatvalue === item.floatvalue &&
+        targetItem.paintseed === item.paintseed &&
+        targetItem.paintindex === item.paintindex
+      ).length;
+
+      const trackedItem = this.trackedItemsRepository.create({
+        originId,
+        destinationId,
+        assetId,
+        market_hash_name: item.market_hash_name,
+        details: {
+          floatValue: item.floatvalue,
+          paintSeed: item.paintseed,
+          paintIndex: item.paintindex,
+        },
+        similarItemsCount,
       });
-      this.logger.log(`Start tracking item ${assetId}`);
+      
+      await this.trackedItemsRepository.save(trackedItem);
+      
+      // Add the tracked item to the in-memory list
+      this.trackedItems.push(trackedItem);
+
+      this.logger.log(`Start tracking item ${assetId} from ${originId} to ${destinationId}`);
     } else {
-      throw new Error(
-        `No item with assetId ${assetId} found in the inventory.`,
-      );
+      throw new Error(`No item with assetId ${assetId} found in the inventory.`);
     }
   }
 
   // To check tracked items periodically
-  async checkTrackedItems(targetId: string) {
-    const targetInventory = await this.getInventory(targetId);
-    this.trackedItems.forEach((item, assetId) => {
-      const itemInOriginInventory = targetInventory.find(
-        (targetItem) =>
-          targetItem.floatvalue === item.floatValue &&
-          targetItem.paintseed === item.paintSeed &&
-          targetItem.paintindex === item.paintIndex,
+  async checkTrackedItems(): Promise<void> {
+    if (this.trackedItems.length === 0) {
+        this.logger.log('No tracked items.');
+        return;
+    }
+
+    this.logger.log(`Checking ${this.trackedItems.length} tracked items.`);
+    
+    for (const trackedItem of this.trackedItems) {
+      const targetInventory = await this.getInventory(trackedItem.destinationId);
+      
+      const similarItemsInTargetInventory = targetInventory.filter((targetItem: targetItem) =>
+        targetItem.market_hash_name === trackedItem.market_hash_name &&
+        targetItem.floatvalue === trackedItem.details.floatValue &&
+        targetItem.paintseed === trackedItem.details.paintSeed &&
+        targetItem.paintindex === trackedItem.details.paintIndex
       );
 
-      if (itemInOriginInventory) {
-        this.logger.log(`Item ${assetId} has moved to the target inventory.`);
-        this.trackedItems.delete(assetId);
+      if (similarItemsInTargetInventory.length > trackedItem.similarItemsCount) {
+        this.logger.log(`Item ${trackedItem.assetId} has moved from ${trackedItem.originId} to ${trackedItem.destinationId}.`);
+        
+        // Remove the item from the in-memory list
+        this.trackedItems = this.trackedItems.filter(item => item.id !== trackedItem.id);
+
+        // Remove the item from the database
+        await this.trackedItemsRepository.remove(trackedItem);
       }
-    });
+    }
   }
 
   // To get the inventory of a user
@@ -99,11 +142,18 @@ export class TrackService {
       console.log('ERRRRR!!!!', error);
     }
   }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async handleCron(): Promise<void> {
+    if (!this.cronEnabled) return this.logger.warn('Cron is disabled.');
+    await this.checkTrackedItems();
+  }
 }
 
-type Item = {
-  floatValue: number;
-  paintSeed: number;
-  paintIndex: number;
-  originId: string;
+type targetItem = {
+    market_hash_name: string;
+    floatvalue: number;
+    paintseed: number;
+    paintindex: number;
 };
+  
