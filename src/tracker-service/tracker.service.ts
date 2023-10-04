bf7d5dbc963fffa49760b37d8688996af3f90067
charliedevcode <charliedevcode@gmail.com>
2023-10-04 12:13:49 +0200

    added data validation

--- src/tracker-service/tracker.service.ts
+++ src/tracker-service/tracker.service.ts
@@ -2,12 +2,13 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
-import { catchError, firstValueFrom /*of*/ } from 'rxjs';
-import { AxiosError } from 'axios';
+// import { catchError, firstValueFrom /*of*/ } from 'rxjs';
+// import { AxiosError } from 'axios';
 import { FloatApiService } from '../float-api/float-api.service';
 import { WalletService } from '../web3-service/wallet.service';
 import { environment as envWeb3 } from '../web3-service/environment';
 import { TrackService } from './track.service';
+import { DatabaseService } from '../database/database/database.service';
 
 @Injectable()
 export class TrackerService {
@@ -17,110 +18,122 @@ export class TrackerService {
     private readonly floatService: FloatApiService,
     private readonly walletService: WalletService,
     private config: ConfigService,
-    private readonly trackService: TrackService
+    private readonly trackService: TrackService,
+    private readonly db: DatabaseService,
   ) {
     this.logger.log('TrackerService dependencies injected');
   }
 
-  async getInventory(_steamId64: string): Promise<any> {
-    try {
-      const { data } = await firstValueFrom(
-        this.httpService
-          .get<InventoryResponse>(
-            `https://api.steamapis.com/steam/inventory/${_steamId64}/730/2?api_key=${this.config.get<string>(
-              'STEAMAPIS_KEY',
-            )}`,
-          )
-          .pipe(
-            catchError((error: AxiosError) => {
-              this.logger.error(error.response.data);
-              throw 'An error happened!x';
-            }),
-          ),
-      );
-      //return data;
-      const merged = [];
-      for (const _asset of data.assets) {
-        //..
-        const description = data.descriptions.find(
-          (desc) =>
-            desc.classid === _asset.classid &&
-            desc.instanceid === _asset.instanceid,
-        );
+  // async getInventory(_steamId64: string): Promise<any> {
+  //   try {
+  //     const { data } = await firstValueFrom(
+  //       this.httpService
+  //         .get<InventoryResponse>(
+  //           `https://api.steamapis.com/steam/inventory/${_steamId64}/730/2?api_key=${this.config.get<string>(
+  //             'STEAMAPIS_KEY',
+  //           )}`,
+  //         )
+  //         .pipe(
+  //           catchError((error: AxiosError) => {
+  //             this.logger.error(error.response.data);
+  //             throw 'An error happened!x';
+  //           }),
+  //         ),
+  //     );
+  //     //return data;
+  //     const merged = [];
+  //     for (const _asset of data.assets) {
+  //       //..
+  //       const description = data.descriptions.find(
+  //         (desc) =>
+  //           desc.classid === _asset.classid &&
+  //           desc.instanceid === _asset.instanceid,
+  //       );
 
-        if (description) {
-          merged.push({
-            appid: _asset.appid,
-            contextid: _asset.contextid,
-            assetid: _asset.assetid,
-            classid: _asset.classid,
-            instanceid: _asset.instanceid,
-            amount: _asset.amount,
-            ...description,
-          });
-        }
-      }
-      return merged;
-    } catch (error) {
-      console.log('ERRRRR!!!!', error);
-    }
-  }
+  //       if (description) {
+  //         merged.push({
+  //           appid: _asset.appid,
+  //           contextid: _asset.contextid,
+  //           assetid: _asset.assetid,
+  //           classid: _asset.classid,
+  //           instanceid: _asset.instanceid,
+  //           amount: _asset.amount,
+  //           ...description,
+  //         });
+  //       }
+  //     }
+  //     return merged;
+  //   } catch (error) {
+  //     console.log('tracker:getInventory ERRRRR!', error);
+  //   }
+  // }
 
-  async runDemo(_steamId64: string) {
-    return await this.getInventory(_steamId64);
-  }
-
-  // 
+  // async runDemo(_steamId64: string) {
+  //   return await this.getInventory(_steamId64);
+  // }
 
   // on BuyerCommitted:
-  // Snapshot inventory of seller. (validate seller has item name and remember count)
-  // Snapshot inventory of buyer (check if buyer has market_hash_name and remember count).
+  // Check if data on-chain and inspect url is the same. ---> If not, cancel trade and refund.
+  
+  // Check if assetId is present in seller inventory. ---> If not, cancel trade and refund.
+  // If Present: 
+  //  Snapshot inventory of buyer (check if buyer has market_hash_name with the same float details and remember the count).
+
+  // on BuyerCancelled:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
 
   // on SellerCommitted:
-  // Snapshot inventory of seller. (validate seller doest not has item anymore)
-  // Snapshot inventory of buyer (check if buyer has market_hash_name and remember count + 1).
+  // Start interval check of buyer inventory (check if buyer has market_hash_name with the same float details and remember the count + 1).
+
+  // on SellerCancelledAfterBuyerCommitted:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // on Completed:
 
   // on SellerConfirmed:
-  // End interval.
+  // ???
 
   // on BuyerConfirmed:
-  // End interval.
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
 
-  // Track Item in Inventory.
-  // Input assetId,
+  //
 
   async onBuyerCommitted(event: any, blockHeight: number): Promise<void> {
-    // Validate if float, paint seed, pattern index is same in contract and inspect url.
-    // If not, cancel trade and refund.
+
+    //Validate if buyer inv is public?
+
 
     //
-    const [isValid, assetId, validationResults] = await this.validateItem(event.contractAddress);
-    console.log('Validation Results', validationResults);
+    // const [isValid, assetId, validationResults] = await this.validateIsItemInfoAndInspectSame(event.contractAddress);
+    // console.log('Validation Results', validationResults);
     
-    if (!isValid) {
-      this.logger.warn('Item is not valid, cancel trade and refund');
-      // TODO: Cancel Trade and Refund call here.
-      return;
-    }
+    // if (!isValid) {
+    //   this.logger.warn('Item is not valid, cancel trade and refund');
+    //   // TODO: Cancel Trade and Refund call here.
+    //   return;
+    // }
 
-    const data = event.data.split('||');
-    const sellerPartnerId = data[0].split('+')[0];
-    const buyerPartnerId = data[1].split('+')[0];
+    // const data = event.data.split('||');
+    // const sellerPartnerId = data[0].split('+')[0];
+    // const buyerPartnerId = data[1].split('+')[0];
 
-    const sellerPartnerIdInt = parseInt(sellerPartnerId);
-    const buyerPartnerIdInt = parseInt(buyerPartnerId);
+    // const sellerPartnerIdInt = parseInt(sellerPartnerId);
+    // const buyerPartnerIdInt = parseInt(buyerPartnerId);
 
-    const sellerSteamId64 = `7656119${sellerPartnerIdInt + 7960265728}`;
-    const buyerSteamId64 = `7656119${buyerPartnerIdInt + 7960265728}`;
+    // const sellerSteamId64 = `7656119${sellerPartnerIdInt + 7960265728}`;
+    // const buyerSteamId64 = `7656119${buyerPartnerIdInt + 7960265728}`;
 
-    this.trackService.trackItem(
-      sellerSteamId64,
-      buyerSteamId64,
-      assetId,
-      validationResults.floatValue.steamValue,
-      validationResults.paintSeed.steamValue,
-      validationResults.paintIndex.steamValue,
-    );
+    // this.trackService.trackItem(
+    //   sellerSteamId64,
+    //   buyerSteamId64,
+    //   assetId,
+    //   validationResults.floatValue.steamValue,
+    //   validationResults.paintSeed.steamValue,
+    //   validationResults.paintIndex.steamValue,
+    // );
 
     // trackItem(originId: string, destinationId: string, assetId: string): Promise<void>
     // originId = seller SteamId64
@@ -189,27 +202,89 @@ export class TrackerService {
     // Snapshot inventory of buyer (check if buyer has market_hash_name and remember count).
   }
 
-  onSellerCommitted(): void {
-    // TODO: Implement method
+  async onSellerCommitted(event: any, blockHeight: number): Promise<void> {
+    console.log('Tracker: onSellerCommitted', event, blockHeight);
+
+    const data = event.data.split('||');
+    const sellerPartnerId = data[0].split('+')[0];
+    const buyerPartnerId = data[1].split('+')[0];
+
+    const sellerPartnerIdInt = parseInt(sellerPartnerId);
+    const buyerPartnerIdInt = parseInt(buyerPartnerId);
+
+    const sellerSteamId64 = `7656119${sellerPartnerIdInt + 7960265728}`;
+    const buyerSteamId64 = `7656119${buyerPartnerIdInt + 7960265728}`;
+
+    // const [isValid, assetId, validationResults] = await this.validateIsItemInfoAndInspectSame(event.contractAddress);
+
+    const cEntity = await this.db.findByContractAddress(event.contractAddress);
+
+    this.trackService.trackItem(
+      event.contractAddress,
+      sellerSteamId64,
+      buyerSteamId64,
+      cEntity.assetId,
+      cEntity.details.floatValue,
+      cEntity.details.paintSeed,
+      cEntity.details.paintIndex,
+    );
   }
 
-  onSellerConfirmed(): void {
-    // TODO: Implement method
+  async onCompleted(event: any, blockHeight: number): Promise<void> {
+    console.log('Tracker: onCompleted', event, blockHeight);
+    this.trackService.removeTrackedItem(event.contractAddress);
   }
 
-  onBuyerConfirmed(): void {
-    // TODO: Implement method
-  }
+  // NEW NEW NEW NEW NEW NEW NEW NEW NEW 
 
-  onSellerCancelledAfterBuyerCommitted(): void {
-    // TODO: Implement method
-  }
+  // on BuyerCommitted:
+  // (here) Check if data on-chain and inspect url is the same. ---> If not, cancel trade and refund.
+  // (in track.ts) Check if assetId is present in seller inventory. ---> If not, cancel trade and refund.
+
+  // on BuyerCancelled:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // on SellerCommitted:
+  // Snapshot inventory of buyer (check if buyer has market_hash_name with the same float details and remember the count).
+  // Start interval of seller inventory (check if assetId is not present anymore)
+  // If not present anymore:
+  // Change status on tracked item in db.
+  // Stop seller interval
+  // Start interval of buyer inventory (check if similarItemsCount + 1).
+
+  // on SellerCancelledAfterBuyerCommitted:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // on Completed:
+  // If they exists:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // on Disputed:
+  // Store dispute in db (isDisputed=true & disputeReason).
+  // If they exists:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // on Resolved:
+  // If they exists:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // on Clawbacked:
+  // If they exists:
+  // Remove trackedItem from in-memory list.
+  // Remove trackedItem from db.
+
+  // ENDOFNEW ENDOFNEW ENDOFNEW ENDOFNEW 
 
   /**
    * Validates if the item info from the contract is the same as the item info from the inspect url.
    */
 
-  async validateItem(contractAddress: string): Promise<[boolean, string, ValidationResults]> {
+  async validateIsItemInfoAndInspectSame(contractAddress: string): Promise<[boolean, string, ValidationResults]> {
     try {
       const contract = await this._getFactoryContract();
   
@@ -221,15 +296,15 @@ export class TrackerService {
   
       const validationResults = this._validateItemInfo(onChainInfo, steamInfo);
   
-      console.log(`Item validation results: ${JSON.stringify(validationResults, null, 2)}`);
+      //console.log(`Item validation results: ${JSON.stringify(validationResults, null, 2)}`);
   
       const isValid = Object.values(validationResults).every((result: ValidationResult) => result.isEqual);
   
-      console.log(`Item validation result: ${isValid ? 'Valid' : 'Invalid'}`);
+      //console.log(`Item validation result: ${isValid ? 'Valid' : 'Invalid'}`);
       return [isValid, chainResults.assetId, validationResults];
     } catch (error) {
-      console.warn(`Failed to validate item ${contractAddress} trying again`, error);
-      this.validateItem(contractAddress);
+      this.logger.error(`Failed to validate item ${contractAddress} trying again`, error);
+      this.validateIsItemInfoAndInspectSame(contractAddress);
     }
   }
   
@@ -279,20 +354,20 @@ export class TrackerService {
   }
 }
 
-type ItemInfo = {
+export type ItemInfo = {
   floatValue: number;
   paintSeed: number;
   paintIndex: number;
   assetId: number;
 };
 
-type ValidationResult = {
+export type ValidationResult = {
   isEqual: boolean;
   onChainValue: number;
   steamValue: number;
 };
 
-type ValidationResults = {
+export type ValidationResults = {
   floatValue: ValidationResult;
   paintSeed: ValidationResult;
   paintIndex: ValidationResult;
